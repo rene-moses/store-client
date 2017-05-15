7c141c7c18a3a0e203f59cd6678d4562042c3a62
Moses Franco <mosesf@outlook.com>
2017-05-15 14:05:10 -0500

    Created basic logic for the sale of a single item

--- src/Store.java
+++ src/Store.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Authored by Moses Franco and Rene Cortez on 5/15/17.
  */
 public class Store {
 	public static void main(String[] args) {
+		System.out.println("1) Sale\n2)Print\n3)Exit");
+		Scanner input = new Scanner(System.in);
+		int menuItem = input.nextInt();
+		String receipt;
 
+		if (menuItem == 1){
+			System.out.println("Type item name: ");
+			String saleItem = input.next();
+			System.out.println("Enter cost of item");
+			double itemPrice = input.nextDouble();
+			System.out.println("Enter the quantity of the item being purchased");
+			int itemQuantity = input.nextInt();
+		}
 	}
 }
