42c48f683e3861a679f508b58692c2d80726df12
Moses Franco <mosesf@outlook.com>
2017-05-15 14:21:12 -0500

    Created methods to handle different parts of the store app

--- src/Store.java
+++ src/Store.java
@@ -5,18 +5,32 @@ import java.util.Scanner;
  */
 public class Store {
 	public static void main(String[] args) {
-		System.out.println("1) Sale\n2)Print\n3)Exit");
 		Scanner input = new Scanner(System.in);
 		int menuItem = input.nextInt();
 		String receipt;
+		double saleTotal;
+		int itemCounter = 1;
 
-		if (menuItem == 1){
+		menuDisplay();
+
+
+	}
+
+	public static void menuDisplay(){
+		System.out.println("1) Sale\n2) Print\n3) Exit");
+	}
+
+	public static double salesDisplay(){
+		int menuItem = input;
+		if(menuItem == 1){
 			System.out.println("Type item name: ");
 			String saleItem = input.next();
 			System.out.println("Enter cost of item");
 			double itemPrice = input.nextDouble();
 			System.out.println("Enter the quantity of the item being purchased");
 			int itemQuantity = input.nextInt();
+			receipt = itemCounter + ") " + itemQuantity + " x " + itemPrice + " " + saleItem;
+			saleTotal = itemPrice * (double) itemQuantity;
 		}
 	}
 }
