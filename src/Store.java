80a2ac36a5c3d30e66e944a3689196735db7ca39
rcortezk9 <rcortezk9@gmail.com>
2017-05-15 15:31:51 -0500

    item counter working on receipt printing

--- src/Store.java
+++ src/Store.java
@@ -8,36 +8,36 @@ public class Store {
 		Scanner input = new Scanner(System.in);
 		String receipt = "";
 		double saleTotal = 0;
+		int itemCounter = 1;
 
-		menuDisplay(input, receipt, saleTotal);
+		menuDisplay(input, receipt, saleTotal, itemCounter);
 
 
 
 	}
 
-	public static void menuDisplay(Scanner input, String receipt, double salesTotal){
+	public static void menuDisplay(Scanner input, String receipt, double salesTotal, int itemCounter){
 		System.out.println("1) Sale\n2) Print\n3) Exit");
 		int menuItem = input.nextInt();
 		if (menuItem ==1){
-			salesDisplay(input, receipt, salesTotal);
+			salesDisplay(input, receipt, salesTotal, itemCounter);
 		} else if (menuItem == 2){
-			printReceipt(input, receipt, salesTotal);
+			printReceipt(input, receipt, salesTotal, itemCounter);
 		} else if (menuItem == 3) {
 			exitSystem();
 		}
 
 	}
 
-	public static void salesDisplay(Scanner input, String receipt, double saleTotal){
+	public static void salesDisplay(Scanner input, String receipt, double saleTotal, int itemCounter){
 
-		int itemCounter = 1;
 		String saleItem = getItemName(input);
 		double itemPrice = getItemPrice(input);
 		int itemQuantity = getItemQuantity(input);
-		receipt = itemCounter + ") " + itemQuantity + " x " + itemPrice + " " + saleItem;
-		saleTotal = itemPrice * (double) itemQuantity;
+		receipt += "\n" + itemCounter + ") " + itemQuantity + " x " + itemPrice + " " + saleItem;
+		saleTotal += itemPrice * (double) itemQuantity;
 		itemCounter++;
-		menuDisplay(input, receipt, saleTotal);
+		menuDisplay(input, receipt, saleTotal, itemCounter);
 
 
 	}
@@ -57,10 +57,10 @@ public class Store {
 		return input.next();
 	}
 
-	public static void printReceipt (Scanner input, String receipt, Double salesTotal){
+	public static void printReceipt (Scanner input, String receipt, Double salesTotal, int itemCounter){
 		System.out.println(receipt);
-		System.out.println(salesTotal);
-		returnMainMenu(input, receipt, salesTotal);
+		System.out.println("$" + salesTotal);
+		returnMainMenu(input, receipt, salesTotal, itemCounter);
 	}
 
 	public static void exitSystem() {
@@ -68,18 +68,18 @@ public class Store {
 		System.exit(0);
 	}
 
-	public static void returnMainMenu (Scanner input, String receipt, double salesTotal){
+	public static void returnMainMenu (Scanner input, String receipt, double salesTotal, int itemCounter){
 		System.out.println("Do you wish to return to the main menu? (y/n)");
 		String continueAnswer = input.next();
 		if ("y".equalsIgnoreCase(continueAnswer)){
-			menuDisplay(input, receipt, salesTotal);
+			menuDisplay(input, receipt, salesTotal, itemCounter);
 		}
 		else if("n".equalsIgnoreCase(continueAnswer)){
 			exitSystem();
 		}
 		else {
 			System.out.println("That's not a proper answer");
-			returnMainMenu(input, receipt, salesTotal);
+			returnMainMenu(input, receipt, salesTotal, itemCounter);
 		}
 	}
 
